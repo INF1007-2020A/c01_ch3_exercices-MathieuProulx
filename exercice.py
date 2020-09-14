31c7b7d286bde9c1d57ec39df0f77d576d6b032c
MathieuProulx <mathieu.proulx@polymtl.ca>
2020-09-14 18:02:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return angle_degs*math.p
+    return angle_degs*math.pi
 
 def to_degrees(angle_rads: float) -> tuple:
     return 0.0, 0.0, 0.0
